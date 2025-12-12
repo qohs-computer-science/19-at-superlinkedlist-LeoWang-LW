6bec78fd0374c3e93db63b9c2fa64383e60415cf
LeoWang-LW <yuzhewang2017@gmail.com>
2025-12-12 17:34:53 +0000

    Finished 12/12/25 CP3

--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -1,3 +1,5 @@
+// Leo Wang || Pd. 6 || 12/12/25 || CP3 
+
 import java.util.LinkedList;
 import java.util.ListIterator;
 
@@ -7,17 +9,14 @@ public class SuperLinkedList extends LinkedList<String> {
          boolean removed = false; 
 
          while(iter.hasNext()){
-            if("AEIOUaeiou".indexOf(iter.next()) != -1)
+            String s = iter.next(); 
+            if("AEIOUaeiou".indexOf(s) != -1){
+                iter.remove(); 
                 removed = true; 
-                iter.remove();
-            }
+            } // end if statement 
+            } // end while statement 
             return removed;
-         }
-         
-
-
-        
-    
+         } // end removedVowels 
 
     public boolean removeConsonants() {
         ListIterator <String> iter = this.listIterator(); 
@@ -27,47 +26,56 @@ public class SuperLinkedList extends LinkedList<String> {
             if("AEIOUaeiou".indexOf(iter.next()) == -1){
                 removed = true; 
                 iter.remove();
-            }
-        }
+            } // end if statement 
+        } // end while statement 
 
         return removed;
-    }
+    } // end removeConsonants 
 
     public LinkedList<String> removeDuplicates() {
-        ListIterator <String> iter1 = this.listIterator(); 
         LinkedList <String> removed = new LinkedList <String>(); 
-        int one = 0; 
 
-        while(iter1.hasNext()){
-            String e = iter1.next(); 
-            ListIterator <String> iter2 = this.listIterator(); 
-            int two = 0; 
+        for(int i = 0; i < this.size(); i++){
+            String current = this.get(i); 
 
-            while(iter2.hasNext()){
-                if(!(two == one) && e.equals(iter2.next()))
-                    removed.add(e);
-                two++;
-            one++;
-            }
+            ListIterator<String> iter = this.listIterator(i+1); 
 
+            while(iter.hasNext()){
+                String val = iter.next(); 
 
-            
-        }
-        return removed;
-    }
+                if(current.equals(val)){
+                    removed.add(val); 
+                    iter.remove(); 
+                } // end if statement 
+            } // end while statement 
+        } // end for loop 
+
+        return removed; 
+    } // end removeDuplicates 
 
     public LinkedList<String> concatenateStrings() {
         LinkedList <String> concatenate = new LinkedList <String>();
+        ListIterator <String> iter = this.listIterator(); 
+        String combine = ""; 
+        while(iter.hasNext()){
+            combine += iter.next(); 
+            concatenate.add(combine); 
+        } // end while 
 
 
-
-        return null;
-    }
+        return concatenate;
+    } // end concatenateStrings 
 
     public LinkedList<String> mix(LinkedList<String> list2) {
-
-        return null;
-    }
+        LinkedList <String> mixedList = new LinkedList <String>(); 
+        ListIterator <String> iter1 = this.listIterator(); 
+        ListIterator <String> iter2 = list2.listIterator(); 
+        while (iter1.hasNext() && iter2.hasNext()){
+            mixedList.add(iter1.next());
+            mixedList.add(iter2.next());
+        } // end while loop 
+        return mixedList;
+    } // end mix
 
     public String toString() {
     ListIterator <String> iter = this.listIterator(); 
@@ -76,7 +84,7 @@ public class SuperLinkedList extends LinkedList<String> {
         current += iter.next(); 
         if(iter.hasNext())
                 current += ", ";
-    }
+    } // end while statement 
         return current;
-    }
+    } // end toString 
 } // end SuperLinkedList
